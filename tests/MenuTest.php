45ce7c079e9c78c5259d677826548c0442b43aa3
poniatowski <poniatowski13@gmail.com>
2022-06-18 13:11:24 +0200

    1. Update .gitignore 2. Install vim git unzip 3. Composer. Install unit test lib.

---
+++ tests/MenuTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Models;
+
+use PHPUnit\Framework\TestCase;
+
+class MenuTest extends TestCase
+{
+    public function testGetProductsByMenuNameOnSuccess(): void
+    {
+        $this->assertSame(1, 1);
+    }
+}
