b433cc8e4a8dcced327a0d6d26a72157133fd48c
Marcin Galaszewski <marcin.galaszewski@festicket.com>
2022-06-18 08:54:47 +0200

    Docker + PHP 8.1 + xdebug

---
+++ public/index.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+$test = 123;
+/*
+interface SomethingInterface
+{
+    public function setClass();
+}
+
+abstract class Base implements SomethingInterface
+{
+    abstract public function setClass();
+}
+
+
+class Triangle extends Base
+{
+    public function setClass(): string
+    {
+        return 'Triangle';
+    }
+}
+
+class Square extends Base
+{
+    public function setClass(): string
+    {
+        return 'Square';
+    }
+}
+
+
+class Factory
+{
+    public function run(SomethingInterface $object): void
+    {
+    }
+}
+
+echo (new Square())->setClass();*/
